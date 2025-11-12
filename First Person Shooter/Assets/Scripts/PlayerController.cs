369d2c3bd33d198eed665406aaadd6fa576f33c1
cwab0 <ivar.lowendahl@gmail.com>
2025-11-12 11:11:36 +0100

    Movement

--- First Person Shooter/Assets/Scripts/PlayerController.cs
+++ First Person Shooter/Assets/Scripts/PlayerController.cs
@@ -1,3 +1,4 @@
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -5,21 +6,35 @@ public class PlayerController : MonoBehaviour
 {
     Transform mainCamera;
     [SerializeField] float mouseSensitivity;
+    [SerializeField] float moveSpeed;
+    [SerializeField] float gravity;
+    [SerializeField] float jumpForce;
 
     float xRotation;
+
+    InputAction moveAction;
+    InputAction jumpAction;
+    CharacterController characterController;
+
+    Vector3 velocity;
 
 
     void Start()
     {
         mainCamera = Camera.main.transform;
+        characterController = GetComponent<CharacterController>();
 
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
+
+        moveAction = InputSystem.actions.FindAction("Move");
+        jumpAction = InputSystem.actions.FindAction("Jump");
     }
 
     void Update()
     {
         HandleLooking();
+        HandleMovement();
     }
 
     void HandleLooking()
@@ -29,5 +44,31 @@ public class PlayerController : MonoBehaviour
         xRotation -= mouseInput.y * mouseSensitivity;
         mainCamera.localRotation = Quaternion.Euler(xRotation, 0, 0);
         transform.Rotate(Vector3.up * mouseInput.x * mouseSensitivity);
+    }
+
+    void HandleMovement()
+    {
+        Vector2 moveVector = moveAction.ReadValue<Vector2>();
+        Vector3 move = transform.right * moveVector.x + transform.forward * moveVector.y;
+        Vector3 horisontalMove = move * moveSpeed;
+
+        Vector3 totalMove = (horisontalMove + velocity) * Time.deltaTime;
+
+        if (characterController.isGrounded)
+        {
+            if(velocity.y < 0)
+            {
+                velocity.y = -2;
+            }
+
+            if (jumpAction.WasPerformedThisFrame())
+            {
+                velocity.y = jumpForce;
+            }
+        }
+
+        velocity.y += gravity * Time.deltaTime;
+
+        characterController.Move(totalMove);
     }
 }
